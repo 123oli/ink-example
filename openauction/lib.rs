49b8caf69967083fa748769016fe0717528b7288
Ganesh oli <ganesh@thaiofficefurniture.com>
2022-12-14 17:09:17 +0545

    open auction wip

--- openauction/lib.rs
+++ openauction/lib.rs
@@ -4,36 +4,116 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod openauction {
-    use ink_storage::Mapping;
+    use ink_storage::{Mapping, traits::SpreadAllocate};
 
 
     #[ink(storage)]
+    #[derive(SpreadAllocate)]
     pub struct Openauction {
         beneficiary: AccountId,
         auction_end_time: Timestamp,
 
-        // Current state of the auction
+        // current state of the auction
         highest_bidder: AccountId,
-        highest_bid: u64,
+        highest_bid: u128,
 
-        // Allowed withdrawals of previous bids
-        pending_returns: Mapping<AccountId, u64>,
+        // allowed withdrawals of previous bids
+        pending_returns: Mapping<AccountId, u128>,
 
-        // Set to true at the end, disallows any change.
-        // By default initialized to `false`
+        // set to true at the end, disallows any change.
+        // by default initialized to `false`
         ended: bool,
     }
 
-    // Events that will be emitted on changes
+    // events that will be emitted on changes
+    #[ink(event)]
+    pub struct HighestBidIncreased {
+        #[ink(topic)]
+        bidder: AccountId,
+        amount: u128
+    }
+
+    #[ink(event)]
+    pub struct AuctionEnded {
+        #[ink(topic)]
+        winner: AccountId,
+        amount: u128
+    }
 
     impl Openauction {
+        /// create a simple auction with `bidding_time`
+        /// seconds bidding time on behalf of the
+        /// beneficiary address `beneficiary`
         #[ink(constructor)]
-        pub fn new() -> Self {
-            Self {}
+        pub fn new(bidding_time: Timestamp, beneficiary: AccountId) -> Self {
+            let now = Self::env().block_timestamp();
+            ink_lang::utils::initialize_contract(|_contract: &mut Openauction|{
+                _contract.beneficiary = beneficiary;
+                _contract.auction_end_time = now + bidding_time;
+            })
         }
 
+        /// bid on the auction with the value sent
+        /// together with the transactions.
+        /// The value will only be refunded if the 
+        /// auction is not won.
+        #[ink(message, payable)]
+        pub fn bid(&mut self) {
+            // revert the call if the bidding period is over
+            let now = self.env().block_timestamp();
+            let caller = self.env().caller();
+            assert!(now <= self.auction_end_time, "auction already ended.");
+
+            // if the bid is not higher, send the money back
+            let balance = self.env().transferred_value();
+            assert!(balance > self.highest_bid, "there already is higher bid.");
+
+            if self.highest_bid != 0 {
+                let mut _pending_returns = self.pending_returns.get(self.highest_bidder).unwrap_or_default();
+                _pending_returns += self.highest_bid;
+            }
+
+            self.highest_bidder = caller;
+            self.highest_bid = balance;
+            self.env().emit_event(HighestBidIncreased{
+                bidder: caller,
+                amount: balance
+            });
+        }
+
+        /// withdraw a bid that was overbid
         #[ink(message)]
-        pub fn get(&mut self) {}
+        pub fn withdraw(&mut self) -> bool {
+            let caller = self.env().caller();
+            let amount = self.pending_returns.get(caller).unwrap_or_default();
+            // it is important to set this to zero because the recipent
+            // can call this function again as part of the receiving call
+            // before `send` returns.
+            if amount > 0 {
+                self.pending_returns.insert(caller, &0);
+
+                // let transfer = self.env().transfer(caller, amount);
+                // TODO:: WIP
+            }
+            true
+        }
+
+        /// end the auction and send the highest bid
+        /// to the beneficiary
+        #[ink(message)]
+        pub fn auction_end(&mut self) {
+            let now = self.env().block_timestamp();
+            assert!(now >= self.auction_end_time, "auction not yet ended.");
+            assert!(!self.ended, "auction end has already been called.");
+
+            self.ended = true;
+            self.env().emit_event(AuctionEnded{
+                winner: self.highest_bidder,
+                amount: self.highest_bid,
+            });
+
+            self.env().transfer(self.beneficiary, self.highest_bid).unwrap_or_default();
+        }
     }
 
 }
